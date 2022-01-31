0195900cc15703ff212dd336cc09307a7bcc9236
Sasha <aleks-one@ukr.net>
2022-01-31 23:18:32 +0200

    Fix errors with linter

--- js/main.js
+++ js/main.js
@@ -1,73 +1,65 @@
-function getNumbr() { 
-    let x = document.getElementById('calc-primary_x').value;
-    let y = document.getElementById('calc-primary_y').value;
-        
-    if (!document.getElementById('calc-primary_x').value.length) {
-        document.getElementById('result-primary').innerHTML = showError();
-        document.getElementById('result-primary').style.color = "red";
-    } else if (!document.getElementById('calc-primary_y').value.length) { 
-        document.getElementById('result-primary').innerHTML = showError();
-        document.getElementById('result-primary').style.color = "red";
-    }
-    else { 
-        document.getElementById('result-primary').innerHTML = showResultNumbr(x, y, calcNumbr(x, y));
-        document.getElementById('result-primary').style.color = "#575757";
-    }
-};
-
-// function calcNumbr(a, b) {
-//     return (b / 100) * a;
-// };
-
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+const showResultNumbr = (a, b, c) => `${a}% от числа ${b} = ${c}`;
+const showError = () => 'Заполните все поля!';
 const calcNumbr = (a, b) => (b / 100) * a;
 
-// function showResultNumbr(a, b, c) {
-//     return `
-//         ${a}% от числа ${b} = ${c}
-//     `;
-// };
+function getNumbr() {
+  const x = document.getElementById('calc-primary_x').value;
+  const y = document.getElementById('calc-primary_y').value;
+  if (!document.getElementById('calc-primary_x').value.length) {
+    document.getElementById('result-primary').innerHTML = showError();
+    document.getElementById('result-primary').style.color = 'red';
+  } else if (!document.getElementById('calc-primary_y').value.length) {
+    document.getElementById('result-primary').innerHTML = showError();
+    document.getElementById('result-primary').style.color = 'red';
+  } else {
+    document.getElementById('result-primary').innerHTML = showResultNumbr(x, y, calcNumbr(x, y));
+    document.getElementById('result-primary').style.color = '#575757';
+  }
+}
 
-const showResultNumbr = (a, b, c) => `${a}% от числа ${b} = ${c}`; 
+const calcPercnt = (a, b) => (a * 100) / b;
+const showResultPercnt = (a, b, c) => `Число ${a} от числа ${b} = ${c}%`;
 
-// function showError() {
-//     return `Заполните все поля!`;
-// };
+function getPercnt() {
+  const x = document.getElementById('calc-secondary_x').value;
+  const y = document.getElementById('calc-secondary_y').value;
 
-const showError = () => `Заполните все поля!`
+  if (!document.getElementById('calc-secondary_x').value.length) {
+    document.getElementById('result-secondary').innerHTML = showError();
+    document.getElementById('result-secondary').style.color = 'red';
+  } else if (!document.getElementById('calc-secondary_y').value.length) {
+    document.getElementById('result-secondary').innerHTML = showError();
+    document.getElementById('result-secondary').style.color = 'red';
+  } else {
+    document.getElementById('result-secondary').innerHTML = showResultPercnt(x, y, calcPercnt(x, y));
+    document.getElementById('result-secondary').style.color = '#575757';
+  }
+}
 
-function getPercnt() { 
-    let x = document.getElementById('calc-secondary_x').value;
-    let y = document.getElementById('calc-secondary_y').value;
-
-    if (!document.getElementById('calc-secondary_x').value.length) {
-        document.getElementById('result-secondary').innerHTML = showError();
-        document.getElementById('result-secondary').style.color = "red";
-    } else if (!document.getElementById('calc-secondary_y').value.length) { 
-        document.getElementById('result-secondary').innerHTML = showError();
-        document.getElementById('result-secondary').style.color = "red";
-    }
-    else { 
-        document.getElementById('result-secondary').innerHTML = showResultPercnt(x, y, calcPercnt(x, y));
-        document.getElementById('result-secondary').style.color = "#575757";
-    }    
-};
+function reset() {
+  document.getElementsByClassName('reset').value = '';
+}
 
 // function calcPercnt(a, b) {
 //     return (a * 100) / b;
 // };
-
-const calcPercnt = (a, b) => (a * 100) / b;
 
 // function showResultPercnt(a, b, c) {
 //     return `
 //         Число ${a} от числа ${b} = ${c}%
 //     `;
 // };
-
-const showResultPercnt = (a, b, c) => `Число ${a} от числа ${b} = ${c}%`;
-
-// function reset() {
-//     document.getElementsByClassName('reset').value = '';
+// function calcNumbr(a, b) {
+//     return (b / 100) * a;
 // };
-
-const reset = () => document.getElementsByClassName('reset').value = '';
+// function showResultNumbr(a, b, c) {
+//     return `
+//         ${a}% от числа ${b} = ${c}
+//     `;
+// };
+// function showError() {
+//     return `Заполните все поля!`;
+// };
+// const reset = () => document.getElementsByClassName('reset').value = '';
